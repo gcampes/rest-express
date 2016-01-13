2c27c8897508436f6e1443a59791a63f047c82d6
Gabriel Campes <gabrieldecampes@gmail.com>
2016-01-13 17:47:38 -0200

    First commit

--- server.js
+++ server.js
@@ -1,6 +1,69 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/emberData');
+app.use(bodyParser());
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
+mongoose.connect('mongodb://localhost/Orbit');
+
+var PersonSchema = new mongoose.Schema({
+	name: 'string',
+	age: 'string',
+});
+
+var PersonModel = mongoose.model('person', PersonSchema);
+
+app.get('/api/people', function(req,res) {
+	PersonModel.find({},function(err,docs) {
+		if(err) {
+			res.send({error:err});
+		}
+		else {
+			res.send({person:docs});
+		}
+	});
+});
+
+app.post('/api/people', function(req, res) {
+  var person = new PersonModel({
+    name: req.body.person.name,
+    age: req.body.person.age
+  });
+
+  person.save(function(err, person){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.end(JSON.stringify(person));
+    }
+  });
+});
+
+app.put('/api/people/:id', function(req, res){
+  var person = new PersonModel({
+    _id: req.query.id,
+    name: req.body.person.name,
+    age: req.body.person.age
+  });
+
+  PersonModel.update(person, function(err, person){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.end(JSON.stringify(person));
+    }
+  })
+})
+
+app.listen('3000');
